066c1e3c038e6645f26cc9ba5cb2cb5a40f6b91e
sarthak gangrade <sarthakgangrade0@gmail.com>
2021-01-08 14:30:17 +0530

    first commit

---
+++ venv/Scripts/activate.bat
@@ -0,0 +1,6 @@
+:: In case user makes changes to PYTHONPATH
+if defined _OLD_VIRTUAL_PYTHONPATH (
+    set "PYTHONPATH=%_OLD_VIRTUAL_PYTHONPATH%"
+) else (
+    set "_OLD_VIRTUAL_PYTHONPATH=%PYTHONPATH%"
+)
